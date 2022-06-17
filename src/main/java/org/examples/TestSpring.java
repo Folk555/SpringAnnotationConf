759e53c2edaf370dee72e7d84cb1d498e1b55e5e
Folk555 <sanya.turulin.98@list.ru>
2022-06-17 19:10:12 +0300

    initial

---
+++ src/main/java/org/examples/TestSpring.java
@@ -0,0 +1,27 @@
+package org.examples;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        //получение контектса по аннотациям
+        //ApplicationContext contextAnnotation = new AnnotationConfigApplicationContext("education.spring");
+
+        //Получение контекста по XML
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("appContext.xml");
+
+        Player pl = (Player) context2.getBean("player");
+        pl.setVolume(10);
+        System.out.println(pl.getVolume());
+        pl.play();
+
+        Player pl2 = (Player) context2.getBean("player");
+        System.out.println(pl2.getVolume());
+
+
+        ((ConfigurableApplicationContext)context2).close();
+    }
+}
